9dce6b4042db8ec5ffd45b6e98395075d550c143
textomatic <102237745+textomatic@users.noreply.github.com>
2022-03-31 13:07:29 +0800

    Updates to pre and in-class learnings

--- day02-if-statements-boolean-operators/in-class/script.js
+++ day02-if-statements-boolean-operators/in-class/script.js
@@ -1,35 +1,46 @@
 var lucky11Main = function (input) {
   // Complete the Base: Lucky 11 exercise below with lucky11Main as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var hawkerFoodCategorisationMain = function (input) {
   // Complete the Base: Hawker Food Categorisation exercise below with hawkerFoodCategorisationMain as the main function.
-  var myOutputValue = 'hello world';
+  // Hawker food is a convenient and affordable food option in Singapore. To help educate non-Singaporeans about hawker food, create an app that takes a hawker dish name as input, and tells users whether the dish has a "rice", "noodle", or "other" base. Some sample dishes are: "chicken rice", "roti prata", "nasi lemak", "hokkien mee", "bak kut teh", and "laksa".
+  // For example, if a user enters "laksa", the program should output "noodle". If a user enters "roti prata", the program could output "other".
+  var myOutputValue = `${input} has neither rice nor noodle base.`;
+  if (input.includes("rice") || input.includes("nasi")) {
+    myOutputValue = `${input} has a rice base.`;
+  } else if (
+    input.includes("mee") ||
+    input.includes("laksa") ||
+    input.includes("noodle")
+  ) {
+    myOutputValue = `${input} has a noodle base.`;
+  }
   return myOutputValue;
 };
 
 var fourDSingleDigitMain = function (input) {
   // Complete the Comfortable: 4D with Single-Digit Comparison exercise below with fourDSingleDigitMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var hawkerFoodRandomnessMain = function (input) {
   // Complete the Comfortable: Hawker Food Randomness exercise below with hawkerFoodRandomnessMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var fourDWinningRangeMain = function (input) {
   // Complete the More Comfortable: 4D with Winning Range exercise below with fourDWinningRangeMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var hawkerFoodOmakaseMain = function (input) {
   // Complete the More Comfortable: Hawker Food Omakase exercise below with hawkerFoodOmakaseMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
--- day02-if-statements-boolean-operators/pre-class/script.js
+++ day02-if-statements-boolean-operators/pre-class/script.js
@@ -1,35 +1,80 @@
-var orangeJuiceMain = function (input) {
+var orangeJuiceMain = function (numGuest) {
   // Attempt the Orange Juice Calculator exercise from the Functions II module below with orangeJuiceMain as the main function.
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var numOranges = 4 * 2 * numGuest;
+  return numOranges;
 };
 
-var paintMain = function (input) {
+var paintMain = function (pricePaintPerLitre) {
   // Attempt the House Paint exercise from the Functions II module below with paintMain as the main function.
-  var myOutputValue = 'hello world';
+
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var randomDiceMain = function (input) {
   // Attempt the Random Dice Rolls exercise from the Intro to Logic and Control Flow module below with randomDiceMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var secretPhraseMain = function (input) {
   // Attempt the Secret Phrase exercise from the If, Else, Else If module below with secretPhraseMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "the door shall not open.";
+  // If input is our secret phrase, change the value of myOutputValue
+  if (
+    input == "palatable papaya" ||
+    input == "neat noodles" ||
+    input == "awesome ayam" ||
+    input == "delicious dumplings"
+  ) {
+    myOutputValue = "you wrote the secret phrase!";
+  } else if (input == "not so palatable papaya") {
+    myOutputValue += " but you are nearly there.";
+  } else {
+    myOutputValue += " nope, this is far from the right phrase.";
+  }
+
   return myOutputValue;
+};
+
+var rollDice = function () {
+  // Generate a decimal from 0 through 6, inclusive of 0 and exclusive of 6.
+  var randomDecimal = Math.random() * 6;
+
+  // Remove the decimal with the floor operation.
+  // This will be an integer from 0 to 5 inclusive.
+  var randomInteger = Math.floor(randomDecimal);
+
+  // Add 1 to get valid dice rolls of 1 through 6 inclusive.
+  var diceNumber = randomInteger + 1;
+
+  return diceNumber;
 };
 
 var diceGameMain = function (input) {
   // Attempt the Dice Game exercise from the If, Else, Else If module below with diceGameMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+  var myOutputValue = "you lose";
+
+  if (
+    randomDiceNumber == input ||
+    randomDiceNumber + 2 == input ||
+    randomDiceNumber - 2 == input
+  ) {
+    myOutputValue = "you win";
+  }
+
   return myOutputValue;
 };
 
 var twiceGuessMain = function (input) {
   // Attempt the Twice the Guess exercise from the If, Else, Else If module below with twiceGuessMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+  var myOutputValue = "you lose";
+
+  if (randomDiceNumber == input * 2) {
+    myOutputValue = "you win";
+  }
+
   return myOutputValue;
 };
